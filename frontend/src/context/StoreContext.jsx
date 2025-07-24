1f96d472890a49c4cc17bad7ee11bf4fcdf167d0
Tanisha Jhalani <tanishajhalani11@gmail.com>
2025-07-25 00:09:48 +0530

    Implemented Cart functionality, Place Order flow, and Login/Sign In pages

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -15,7 +15,7 @@ const addToCart = (itemId)=>{
       setCartItems((prev)=>({...prev , [itemId]:1}))
     }
     else{
-      setCartItems((prev)=>({...prev , [itemId]:prev[itemId]+1}))
+      setCartItems((prev)=>({...prev , [itemId]:prev[itemId]+1})) 
     }
 }
 
@@ -23,15 +23,25 @@ const removeFromCart = (itemId)=>{
       setCartItems((prev)=>({...prev ,[itemId]:prev[itemId]-1}))
 }
 
-useEffect(()=>{
-  console.log(cartItems)
-},[cartItems])
+const getTotalCartAmount = ()=>{
+  let totalAmount = 0 ;
+  for(const item in cartItems ){
+    if(cartItems[item] > 0){
+    let itemInfo = food_list.find((product)=>product._id===item);
+    totalAmount += itemInfo.price* cartItems[item] ;
+    }
+    
+  }
+
+  return totalAmount ;
+}
   const contextValue = {
       food_list , 
       cartItems , 
       setCartItems,
       addToCart,
-      removeFromCart
+      removeFromCart,
+      getTotalCartAmount
   }
   return (
     <StoreContext.Provider value = {contextValue}>
