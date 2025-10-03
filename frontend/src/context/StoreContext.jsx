10b176b56d22f23edaaf22681f4067c620f65a4d
Tanisha Jhalani <127257931+Tanisha-Jhalani@users.noreply.github.com>
2025-10-04 04:12:46 +0530

    Update StoreContext.jsx

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -9,7 +9,7 @@ export const StoreContext = createContext(null);
 const StoreContextProvider = (props)=>{
 
 const [cartItems , setCartItems] = useState({});
-const url = "http://localhost:4000"
+const url = "https://delivery-website-backend-ssbp.onrender.com"
 const[token , setToken] = useState("") ;
 const [food_list , setFoodList] = useState([])
 const addToCart = async(itemId)=>{
